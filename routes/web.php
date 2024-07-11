7df94cd9b0f361997db227a3a9aee46b6e66fb77
Sanjay <sanjaypanihar@gmail.com>
2024-07-11 22:24:34 +0530

    register page, password reset page add

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
